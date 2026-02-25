5d1cfe415d9e547f6e38bd68dda6dc3b14943053
DadhichTanishka <tanishka.dadhich@kalvium.community>
2026-02-25 15:25:21 +0530

    setup a basic rust server

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,3 +1,20 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{routing::get, Router};
+use std::net::SocketAddr;
+
+async fn root() ->&'static str {
+    "IntegrityX API Running 🚀"
+}
+
+#[tokio::main]
+async fn main(){
+    let app = Router::new().route("/",get(root));
+    let addr = SocketAddr::from(([127,0,0,1],3000));
+    println!("Server is running on {}",addr);
+
+    let listner = tokio::net::TcpListener::bind(addr)
+    .await
+    .unwrap();
+    axum::serve(listner, app)
+    .await
+    .unwrap();
 }
